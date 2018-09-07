b76bbf01c9dc4c3da5cb6f39e46e1e60ebf13e04
unknown <benammar.khadija08@gmail.com>
2018-09-07 16:40:26 +0100

    done

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ import keras
 from flask import request
 from keras.models import load_model
 
+import shutil
+import os
+import random
+
 # instantiate flask
 app = flask.Flask(__name__)
-
+app.url_map.strict_slashes = False
 
 
 # we need to redefine our metric function in order
@@ -22,17 +26,46 @@ def auc(y_true, y_pred):
 # load the model, and pass in the custom metric function
 global graph
 graph = tf.get_default_graph()
-model = load_model('model31.h5', custom_objects={'auc': auc})
+model = load_model('test.h5', custom_objects={'auc': auc})
 
 
-@app.route("/api/getPath", methods=['POST','GET'])
+@app.route("/api/pathTestDada", methods=['POST'])
+def pathTestDada():
+    if request.method == 'POST':
+
+        audio = request.get_json(silent=True)
+        print(audio["path"])
+
+        re = predictTest(audio["path"])
+        return (re)
+
+
+@app.route("/api/getPath", methods=['POST'])
 def getPath():
     if request.method == 'POST':
 
         audio = request.get_json(silent=True)
         print(audio["path"])
-        re=predict(audio["path"])
+
+        re = predict(audio["path"])
+        global chemin
+        chemin = audio["path"]
+        print(chemin)
         return (re)
+
+
+
+@app.route("/api/save_audio", methods=['POST'])
+def saveAudio():
+    if request.method == 'POST':
+        print(chemin)
+        dest = request.get_json(silent=True)
+        destination = dest["path"]
+        source = "C://Users//Stage//Downloads//"+chemin+".wav"
+        dest1 = "C://Users//Stage//final project//test//"+destination
+
+        shutil.move(source, dest1)
+        return (dest1)
 
 
 
@@ -48,13 +81,42 @@ def predict(name):
     # if parameters are found, return a prediction
     if (params != None):
         with graph.as_default():
-            sample = preprocess.wav2mfcc('./test/'+name+'/00f0204f_nohash_0.wav')
-            sample_reshaped = sample.reshape(1, 40, 249, 1)
+            sample = preprocess.wav2mfcc('C://Users//Stage//Downloads//'+name+'.wav')
+            print(name)
+            sample_reshaped = sample.reshape(1, 40, 47, 1)
             data["prediction"] = preprocess.get_labels()[0][np.argmax(model.predict(sample_reshaped))]
             data["success"] = True
 
     # return a response in json format
     return flask.jsonify(data)
+
+
+
+
+
+def predictTest(name):
+
+    data = {"path": name}
+
+    params = flask.request.json
+    if (params == None):
+        params = flask.request.args
+
+    # if parameters are found, return a prediction
+    if (params != None):
+        with graph.as_default():
+            dir= "C://Users//Stage//final project//test//"+name
+            filename = random.choice(os.listdir(dir))
+            print (filename)
+            sample = preprocess.wav2mfcc(dir+"//"+filename)
+            print(name)
+            sample_reshaped = sample.reshape(1, 40, 47, 1)
+            data["prediction"] = preprocess.get_labels()[0][np.argmax(model.predict(sample_reshaped))]
+            data["success"] = True
+
+    # return a response in json format
+    return flask.jsonify(data)
+
 
 
 # start the flask app, allow remote connections
--- preprocess.py
+++ preprocess.py
@@ -8,30 +8,46 @@ import matplotlib.pyplot as plt
 import librosa.display
 
 DATA_PATH = "./data/"
-feature_dim_1 = 40
-channel = 1
-epochs = 50
-batch_size = 30
-verbose = 1
-num_classes = 30
-feature_dim_2 = 249
+DATA_PATH1 = "./test/"
+
 
 # Input: Folder Path
 # Output: Tuple (Label, Indices of the labels, one-hot encoded labels)
 def get_labels(path=DATA_PATH):
     labels = os.listdir(path)
     label_indices = np.arange(0, len(labels))
+    print(len(labels))
     return labels, label_indices, to_categorical(label_indices)
 
 
 # convert wav to mfcc vectors
-def wav2mfcc(file_path, max_len=249):
-    wave, sr = librosa.load(file_path, mono=True, sr=None) #monaural / stereophonic sound (un seul channel/+ieurs channels)
-    #sr=44.1khz
-    mfcc = librosa.feature.mfcc(wave, sr, n_mfcc=40, n_fft=1764,hop_length=882)  #sampling rate / default hop value 512
+def wav2mfcc(file_path, max_len=47):
+    wave, sr = librosa.load(file_path, mono=True,
+                            sr=None)  # monaural / stereophonic sound (un seul channel/+ieurs channels)
+    # sr=44.1khz
 
+    mfcc = librosa.feature.mfcc(wave, sr, n_mfcc=40)  # sampling rate / default hop value 512
+    # taaille de la fenetre=40ms
+    # default=2048 " a recording with a duration of 60 seconds will contain 2,646,000 samples."
+
+    # Encode redundancy in MFCCs: By overlapping frames (at > 50 %) we have increased redundancy in the MFCCs. However, this redundancy can be exploited in improved delta-MFCCs and delta-delta-MFCCs computation as we will have a more smooth difference function approximation
+
+    # A ce niveau il faut définir une taille générale pour tous les audios pour que ConvNet fonctionne correctemen
+    # si la longueur de l'audio est < à la longueure maximale on termine par 0
+    # Sinon on coupe l'audio
+
+    # 1er cas
+    # print("shape")
+    # print(mfcc.shape)
+
+    # plt.figure(figsize=(10, 4))
+    # librosa.display.specshow(mfcc, x_axis='time')
+    # plt.colorbar()
+    # plt.title('MFCC')
+    # plt.tight_layout()
 
     if (max_len > mfcc.shape[1]):
+
         pad_width = max_len - mfcc.shape[1]
         mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
 
@@ -42,7 +58,9 @@ def wav2mfcc(file_path, max_len=249):
     return mfcc
 
 
-def save_data_to_array(path=DATA_PATH, max_len=249):
+# on va maintenant définir une fonction qui va lire tous les fichiers audios dans un dossier donné et transformer chaque audio en des vecteurs et les sauvegarder dans un fichier .npy en lui donnant le nom du label
+
+def save_data_to_array(path, max_len=47):
     labels, _, _ = get_labels(path)
 
     for label in labels:
@@ -56,15 +74,15 @@ def save_data_to_array(path=DATA_PATH, max_len=249):
         np.save(label + '.npy', mfcc_vectors)
 
 
+# Dans cette partie, nous allons diviser notre dataset en training et testing parts.
+# nous allons utiliser la fonction train_test_split de la librairie sklearn qui va générer automatiquement cette division
 
-def get_train_test(split_ratio=0.7):
+def get_train(split_ratio=1):
     # Get available labels
     labels, indices, _ = get_labels(DATA_PATH)
 
     # Getting first arrays
     X = np.load(labels[0] + '.npy')
-    print("train");
-    print(X.shape)
     y = np.zeros(X.shape[0])
 
     # Append all of the dataset into one single array, same goes for y
@@ -75,16 +93,28 @@ def get_train_test(split_ratio=0.7):
         y = np.append(y, np.full(x.shape[0], fill_value=(i + 1)))
 
     assert X.shape[0] == len(y)
+    print("train")
     print(len(y))
 
-    return train_test_split(X, y, test_size=(1 - split_ratio), shuffle=True)
+    return (X, y)
 
 
-def predictt(filepath, model):
-    sample = wav2mfcc(filepath)
-    sample_reshaped = sample.reshape(1, feature_dim_1, feature_dim_2, 1)
+def get_test():
+    # Get available labels
+    labels, indices, _ = get_labels(DATA_PATH1)
 
-    print(get_labels()[0][
-            np.argmax(model.predict(sample_reshaped))
-        ])
-    return "hii"
+    # Getting first arrays
+    X = np.load(labels[0] + '.npy')
+    y = np.zeros(X.shape[0])
+
+    # Append all of the dataset into one single array, same goes for y
+    for i, label in enumerate(labels[1:]):
+        x = np.load(label + '.npy')
+
+        X = np.vstack((X, x))  # vertical
+        y = np.append(y, np.full(x.shape[0], fill_value=(i + 1)))
+
+    assert X.shape[0] == len(y)
+    print("test")
+    print(len(y))
+    return (X, y)
